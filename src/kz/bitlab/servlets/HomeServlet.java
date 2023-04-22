dd9e6219115d997d657624323100a67d155bb50e
Daniyar <daniyar.doskulov@narxoz.kz>
2023-04-22 12:42:04 +0600

    first commit

---
+++ src/kz/bitlab/servlets/HomeServlet.java
@@ -0,0 +1,20 @@
+package kz.bitlab.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(value = "/home")
+public class HomeServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("<h1>Hello BITLAB</h1>");
+    }
+}
